5e4716ef21eee9aee21718337243b280b0ec9870
katspaugh <katspaugh@gmail.com>
2024-02-29 13:28:01 +0100

    Resize from bottom and right

--- components/ResizeHandle.js
+++ components/ResizeHandle.js
@@ -2,16 +2,12 @@ import { makeDraggable } from '../utils/draggable.js'
 
 export function ResizeHandle({ onResize, onResizeEnd }) {
   const container = document.createElement('div')
+  container.className = 'resize-handle'
 
-  Object.assign(container.style, {
-    position: 'absolute',
-    right: 0,
-    bottom: 0,
-    width: '10px',
-    height: '10px',
-    background: 'linear-gradient(135deg, transparent 0%, transparent 50%, #999 50%, #999 100%)',
-    cursor: 'nwse-resize',
-  })
+  for (let i = 0; i < 2; i++) {
+    const div = document.createElement('div')
+    container.appendChild(div)
+  }
 
   if (onResize) {
     makeDraggable(container, onResize, undefined, onResizeEnd)
