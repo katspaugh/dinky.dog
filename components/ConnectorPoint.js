c1b85bd2eefe76deb5af7be03e107bcf782073fb
katspaugh <katspaugh@gmail.com>
2024-03-06 22:03:56 +0100

    Sine cursor for connectors

--- components/ConnectorPoint.js
+++ components/ConnectorPoint.js
@@ -13,6 +13,7 @@ export function ConnectorPoint(left, top) {
     zIndex: 5,
     left,
     top,
+    cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>∿</text></svg>") 0 16, pointer`,
   })
 
   return {
