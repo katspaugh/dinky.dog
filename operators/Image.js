a36b20635c894a41ea7e323b3872ffe148165fc8
katspaugh <katspaugh@gmail.com>
2024-03-18 22:04:47 +0100

    Rm input from the Image

--- operators/Image.js
+++ operators/Image.js
@@ -1,18 +1,10 @@
 import { Stream } from '../utils/stream.js'
-import { parseImageUrl } from '../utils/parse-text.js'
 import { ImagePreview } from '../components/ImagePreview.js'
 
 export function Image(src = '') {
   const inputStream = new Stream()
   const outputStream = new Stream()
   const component = ImagePreview()
-
-  inputStream.subscribe((text) => {
-    const url = parseImageUrl(src)
-    if (url) {
-      outputStream.next(url)
-    }
-  })
 
   outputStream.subscribe((src) => {
     component.render({ src })
