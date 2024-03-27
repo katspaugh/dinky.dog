78ad29c4382deabdaec9c08bfbc09d51106bad90
katspaugh <katspaugh@gmail.com>
2024-03-27 22:31:54 +0100

    Bg container 100%

--- components/DropContainer.js
+++ components/DropContainer.js
@@ -2,7 +2,12 @@ import { Component } from '../utils/dom.js'
 import { makeDroppable } from '../utils/droppable.js'
 
 export function DropContainer({ fileTypes, onDrop }) {
-  const component = Component({})
+  const component = Component({
+    style: {
+      width: '100%',
+      height: '100%',
+    },
+  })
 
   makeDroppable({
     container: component.container,
