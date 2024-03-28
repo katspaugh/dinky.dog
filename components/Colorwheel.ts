e560c124f88c8990be0a6d018dd9957cf202b645
katspaugh <katspaugh@gmail.com>
2024-03-28 21:47:39 +0100

    Restore previous colorwheel palette

--- components/Colorwheel.ts
+++ components/Colorwheel.ts
@@ -3,15 +3,15 @@ import { Component, el } from '../utils/dom.js'
 // Paster colors
 const PASTEL_COLORS = [
   '#F9F9F9',
-  '#EDF9FF',
-  '#FBE9ED',
+  '#ffe8ed',
+  '#d6e2e7',
+  '#bbeebb',
+  '#fefeca',
+  '#e1d8f0',
+  '#ffd9a3',
+  '#d9f3e6',
   '#BFE8FE',
-  '#FFFFE9',
-  '#E8FFE9',
-  '#E7FFFF',
-  '#FADAAA',
-  '#E0D9EE',
-  '#C6ECC0',
+  '#fff2d9',
 ]
 
 // Share the same datalist for all colorwheels
